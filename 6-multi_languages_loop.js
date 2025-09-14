2f086ec12ca9c82c8da3867845bf3f4f1c116115
Ali-Abdalgalil <aliabdalgalil10@gmail.com>
2025-09-14 20:36:55 +0300

    Add task 6

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+const languages = [
+    "C is fun",
+    "Python is cool",
+    "JavaScript is amazing"
+];
+
+const out = [];
+for (const line of languages) {
+    out.push(line);
+}
+
+console.log(out.join("\n"));
